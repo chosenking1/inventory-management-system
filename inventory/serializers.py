1d2ca3c92eb78dbead70f2cb090a5689c07e12c9
chosenking1 <joshua.sofowora@gmail.com>
2024-07-25 21:59:25 +0100

    App set-up

---
+++ inventory/serializers.py
@@ -0,0 +1,54 @@
+# inventory/serializers.py
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+
+User = get_user_model()
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'email', 'role')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            role=validated_data['role'],
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+# inventory/serializers.py
+from rest_framework import serializers
+from .models import Product, Order, OrderItem
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = ['product', 'quantity']
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True)
+
+    class Meta:
+        model = Order
+        fields = ['id', 'user', 'items', 'status', 'created_at', 'updated_at']
+
+    def create(self, validated_data):
+        items_data = validated_data.pop('items')
+        order = Order.objects.create(**validated_data)
+        for item_data in items_data:
+            OrderItem.objects.create(order=order, **item_data)
+        return order
---
+++ inventory/urls.py
@@ -0,0 +1,19 @@
+# inventory/urls.py
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import RegisterView, LoginView, ProductViewSet, OrderViewSet, LowStockReportView, SalesReportView
+
+
+
+router = DefaultRouter()
+router.register(r'products', ProductViewSet)
+router.register(r'orders', OrderViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+     path('report/low_stock/', LowStockReportView.as_view(), name='low_stock_report'),
+    path('report/sales/', SalesReportView.as_view(), name='sales_report'),
+
+]
